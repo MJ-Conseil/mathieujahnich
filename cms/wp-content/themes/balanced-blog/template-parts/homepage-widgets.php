d8e03f57125a2ad6a31bedb27f03db35967d65af
volubyl <volubyl@pm.me>
2022-12-05 08:35:45 +0100

    reorganize code to be deployed with jelastic

--- cms/wp-content/themes/balanced-blog/template-parts/homepage-widgets.php
+++
@@ -1,8 +0,0 @@
-<?php if ( is_active_sidebar( 'balanced-blog-homepage-area' ) ) { ?>
-	<div class="homepage-widget-area"> 
-		<?php
-		dynamic_sidebar( 'balanced-blog-homepage-area' );
-		?>
-	</div>
-<?php
-}
