ba43d841354af11ff0ba6b1322430b1e5ab74445
volubyl <volubyl@pm.me>
2022-12-23 12:17:30 +0100

    chore: add tailwind configuration

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,7 +7,12 @@ module.exports = {
 				light: '#F2F3F7',
 				dark: '#06154A'
 			},
-			sand: '#F0CB85'
+			sand: '#F0CB85',
+			indigo: '#291F9C'
+		},
+		fontFamily: {
+			ptsans: ['PT Sans', 'sans-serif'],
+			sans: ['Arial', 'sans-serif']
 		},
 		extend: {}
 	},
