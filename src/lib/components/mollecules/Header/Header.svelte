673a9ec7864856ba459f08cce2e4366e4197eb11
volubyl <volubyl@pm.me>
2023-01-03 09:54:04 +0100

    rework html structure

--- src/lib/components/mollecules/Header/Header.svelte
+++ src/lib/components/mollecules/Header/Header.svelte
@@ -2,8 +2,9 @@
 	import Icon from '$lib/components/atoms/Icon/Icon.svelte';
 </script>
 
-<header class="flex justify-between">
+<div class="flex justify-between">
 	MJ conseil
-
 	<Icon height="2rem" width="2rem" name="burger" />
-</header>
+
+	<!-- Navigation will be around there -->
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,36 +4,41 @@
 
 <Header />
 
-<main class="bg-blue-dark mt-3" id="main-content">
-	<h1 class="mj-h1--alt">Nous mettons la communication au service de la transition écologique</h1>
+<div class="bg-blue-dark mt-3" id="main-content">
+	<!-- svelte-ignore a11y-no-redundant-roles -- this role is not redudant because this is the main page header -->
+	<header role="banner">
+		<h1 class="mj-h1--alt">Nous mettons la communication au service de la transition écologique</h1>
 
-	<p class="mt-3 text-white">
-		Nous sommes un trio d’experts en RSE et communication capables de vous aider de la définition de
-		votre stratégie à la mise en place opérationnelle.
-	</p>
+		<p class="mt-3 text-white">
+			Nous sommes un trio d’experts en RSE et communication capables de vous aider de la définition
+			de votre stratégie à la mise en place opérationnelle.
+		</p>
+	</header>
+</div>
+
+<main id="main">
+	<section class="mt-3">
+		<h2>Signes particuliers</h2>
+	</section>
+
+	<section class="mt-3">
+		<h2>Nos offres et services</h2>
+	</section>
+
+	<section class="mt-3">
+		<h2>Formez-vous</h2>
+	</section>
+
+	<section class="mt-3">
+		<h2>A la une du blog</h2>
+
+		<p class="font-ptsans text-2xl">
+			Depuis 2005, j’interroge l’alignement de la communication et du marketing avec les enjeux de
+			transition écologique. Retrouvez ici mes analyses, des témoignages et des ressources. Ces
+			contenus étaient précédemment publiés sur le site Sircome.
+		</p>
+	</section>
 </main>
-
-<section class="mt-3">
-	<h2>Signes particuliers</h2>
-</section>
-
-<section class="mt-3">
-	<h2>Nos offres et services</h2>
-</section>
-
-<section class="mt-3">
-	<h2>Formez-vous</h2>
-</section>
-
-<section class="mt-3">
-	<h2>A la une du blog</h2>
-
-	<p class="font-ptsans text-2xl">
-		Depuis 2005, j’interroge l’alignement de la communication et du marketing avec les enjeux de
-		transition écologique. Retrouvez ici mes analyses, des témoignages et des ressources. Ces
-		contenus étaient précédemment publiés sur le site Sircome.
-	</p>
-</section>
 
 <!-- svelte-ignore a11y-no-redundant-roles not redundant because this is the main footer -->
 <footer role="contentinfo" class="mt-5">
