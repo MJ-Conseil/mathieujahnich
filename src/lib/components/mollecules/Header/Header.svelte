bc62abb0bac5384dd17960c377ca0d3d8c9155c6
Volubyl <volubyl@pm.me>
2023-01-03 14:30:13 +0100

    feat: add basic landmark for home page (#32)

---
+++ src/lib/components/mollecules/Header/Header.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Icon from '$lib/components/atoms/Icon/Icon.svelte';
+</script>
+
+<div class="flex justify-between">
+	MJ conseil
+	<Icon height="2rem" width="2rem" name="burger" />
+
+	<!-- Navigation will be around there -->
+</div>
