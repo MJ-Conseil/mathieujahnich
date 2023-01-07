25a0cd2030d34a35f3c79ecffaccd6f69a7b01ae
julie-desvaux <desvaux.julie@gmail.com>
2023-01-07 20:43:55 +0100

    Add offers

--- src/lib/components/atoms/Circle/Circle.svelte
+++ src/lib/components/atoms/Circle/Circle.svelte
@@ -5,6 +5,7 @@
 	export let size: Size = SIZE.regular;
 </script>
 
+<<<<<<< HEAD
 <div class={`bg-sand rounded-full mx-auto ${size}`} />
 
 <style>
@@ -13,3 +14,6 @@
 		width: 8.5rem;
 	}
 </style>
+=======
+<div class="bg-sand rounded-full" style=" width: {size}; height: {size}" />
+>>>>>>> 95218574 (Add offers)
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
-<<<<<<< HEAD
+
 	import Icon from '$lib/components/atoms/Icon/Icon.svelte';
 	import Footer from '$lib/components/mollecules/Footer/Footer.svelte';
 	import Offer from '$lib/components/mollecules/Offer/Offer.svelte';
-=======
-	import Circle from '$lib/components/atoms/Circle/Circle.svelte';
-	import Header from '$lib/components/mollecules/Header/Header.svelte';
->>>>>>> 0f4a463c (Add Circle)
+
 </script>
 
 <svelte:head>
@@ -71,11 +68,11 @@
 	</section>
 
 	<section class="my-4">
-		<h2 class="mb-8">Formez-vous</h2>
+		<h2 class="mb-4 font-bold">Formez-vous</h2>
 	</section>
 
 	<section class="my-4">
-		<h2 class="mb-8">A la une du blog</h2>
+		<h2 class="mb-4 font-bold">A la une du blog</h2>
 
 		<p class="font-ptsans text-2xl">
 			Depuis 2005, j’interroge l’alignement de la communication et du marketing avec les enjeux de
@@ -85,16 +82,4 @@
 	</section>
 </main>
 
-<<<<<<< HEAD
 <Footer />
-=======
-<!-- svelte-ignore a11y-no-redundant-roles not redundant because this is the main footer -->
-<footer role="contentinfo" class="mt-5">
-	<nav>
-		<ul>
-			<li><a href="#"> lien 1</a></li>
-			<li><a href="#"> lien 2</a></li>
-		</ul>
-	</nav>
-</footer>
->>>>>>> 0f4a463c (Add Circle)
