43cc9251d3d20de83de3d2ddd0f899b63e715c4a
volubyl <volubyl@pm.me>
2023-01-07 18:04:49 +0100

    chore: configure playwright for a11 tests

---
+++ e2e/a11y.spec.ts
@@ -0,0 +1,14 @@
+import { test } from './fixtures.js';
+import { expect } from '@playwright/test';
+
+test.describe('accessibilty - pages without required authentication', () => {
+	test('home page should not have any automatically detectable accessibility issues', async ({
+		page,
+		makeAxeBuilder
+	}) => {
+		await page.goto('/');
+
+		const accessibilityScanResults = await makeAxeBuilder().analyze();
+		expect(accessibilityScanResults.violations).toEqual([]);
+	});
+});
