060e47f58391e2f4b00be6f47b26f19cab198201
volubyl <volubyl@pm.me>
2023-01-08 15:48:15 +0100

    add footer mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Icon from '$lib/components/atoms/Icon/Icon.svelte';
+	import { SOCIAL_NETWORK_LINKS } from '$lib/constants';
 </script>
 
 <svelte:head>
@@ -54,3 +55,66 @@
 		</p>
 	</section>
 </main>
+
+<!-- svelte-ignore a11y-no-redundant-roles not redundant because this is the main footer -->
+<footer role="contentinfo" class="bg-blue-dark mt-5 md:flex p-4">
+	<div>
+		<h3 class="mj-h3--alt my-2">Offre conseil</h3>
+		<ul class="gap-3 flex flex-col">
+			<li><a class="mj-link--alt visited:underline" href="/tata">Prendre de la hauteur</a></li>
+			<li><a class="mj-link--alt visited:underline" href="/toto">Trouver le juste équilibre</a></li>
+			<li><a class="mj-link--alt visited:underline" href="/tutu">Donner de l'élan</a></li>
+		</ul>
+	</div>
+
+	<div class="">
+		<h3 class="mj-h3--alt my-2">Contact</h3>
+		<ul class="gap-1 flex flex-col">
+			<li class="flex text-white">
+				<span aria-hidden="true">
+					<Icon class="pr-1 text-sand" width="18px" height="auto" name="phone" />
+				</span>
+
+				<span class="sr-only">téléphone :</span>
+				+33 6 74 88 92 73
+			</li>
+			<li class="flex text-white">
+				<span aria-hidden="true">
+					<Icon class="pr-1 text-sand" width="18px" height="auto" name="letter" />
+				</span>
+				<a class="text-white" href="mailto:conseil@mathieu-jahnich.fr">
+					<span class="sr-only">email :</span>
+					conseil@mathieu-jahnich.fr
+				</a>
+			</li>
+		</ul>
+
+		<ul class="mt-4 flex gap-2">
+			<li>
+				<a href={SOCIAL_NETWORK_LINKS.linkedin}>
+					<span aria-hidden="true">
+						<Icon name="linkedin" width="25px" height="auto" class="text-sand" /></span
+					>
+					<span class="sr-only">Linkedin</span>
+				</a>
+			</li>
+		</ul>
+	</div>
+
+	<hr class="text-white my-4" />
+
+	<div class="text-white">
+		<p>© Copyright 2005 – 2022 | Tous droits réservés</p>
+	</div>
+
+	<nav class="mt-4">
+		<ul>
+			<li class="text-white">
+				Conception et création : <a
+					class="text-white decoration-dotted"
+					href="https://www.fairness.coop">Fairness Scop</a
+				> et les Designers Ethiques
+			</li>
+		</ul>
+	</nav>
+</footer>
