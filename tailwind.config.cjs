3181c56d84bf993064cf256fa28491ab3e6c6e20
volubyl <volubyl@pm.me>
2023-01-10 14:22:57 +0100

    add tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		colors: {
+			blue: {
+				light: '#F2F3F7',
+				dark: '#06154A'
+			},
+			sand: '#F0CB85',
+			indigo: '#291F9C',
+			white: '#ffffff'
+		},
+		fontFamily: {
+			ptsans: ['PT Sans', 'sans-serif'],
+			sans: ['Arial', 'sans-serif']
+		},
+		extend: {}
+	},
+	plugins: []
+};
