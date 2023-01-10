89ecce0e2bb12dd9f748f54f3c7550574f435c64
volubyl <volubyl@pm.me>
2023-01-10 14:59:30 +0100

    feat: add unit tests

--- src/lib/components/mollecules/Footer/Footer.test.ts
+++ src/lib/components/mollecules/Footer/Footer.test.ts
@@ -6,8 +6,15 @@ import { render, screen } from '@testing-library/svelte';
 import Footer from './Footer.svelte';
 
 test('shows proper heading when rendered', () => {
-    render(Footer);
-    const headings = screen.getAllByRole('heading');
+	render(Footer);
+	const headings = screen.getAllByRole('heading');
 
-    expect(headings.length).toBe(2)
+	expect(headings.length).toBe(2);
+	expect(headings[0]).toHaveTextContent('Offre conseil');
+	expect(headings[1]).toHaveTextContent('Contact');
+});
+
+test('shows proper catch phrase when rendered', () => {
+	render(Footer);
+	screen.getByText('Nous mettons la communication au service de la transition écologique');
 });
