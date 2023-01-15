9c6daa0279ca002d4d7299d761df609a558d0dbb
Volubyl <volubyl@pm.me>
2023-01-15 16:06:25 +0100

    fix: home page style improvement (#47)

---
+++ src/lib/components/mollecules/CircledIcon/CircledIcon.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Circle from '$lib/components/atoms/Circle/Circle.svelte';
+	import Icon from '$lib/components/atoms/Icon/Icon.svelte';
+	import type { IconNames } from 'src/definitions/Icon';
+	export let iconName: IconNames;
+</script>
+
+<div aria-hidden="true" class="relative h-[200px]">
+	<Circle size="regular" />
+	<div class="absolute  top-0 left-[40px]">
+		<Icon height="15rem" width="18rem" name={iconName} fill="#1E2345" />
+	</div>
+</div>
