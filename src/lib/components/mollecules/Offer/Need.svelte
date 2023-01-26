1b83ceeeda685bc9a90b973b424df06f0502486b
julie-desvaux <desvaux.julie@gmail.com>
2023-01-26 18:17:18 +0100

    Beggin Offer Page : Prendre de la hauteur

---
+++ src/lib/components/mollecules/Offer/Need.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import Circle from '$lib/components/atoms/Circle/Circle.svelte';
+	export let title: string;
+	export let contents: string[];
+</script>
+
+<div class="bg-gray p-6 relative rounded-xl">
+	<div class=" absolute top-4">
+		<Circle size="small" />
+	</div>
+
+	<h3 class="my-6 mj-h4">{title}</h3>
+	<ul class="mj-list--disc">
+		{#each contents as content}
+			<li>{content}</li>
+		{/each}
+	</ul>
+</div>
