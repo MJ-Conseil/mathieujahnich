18c422fa6ad4b98d7e7058d9882eeb4e60150c9e
julie-desvaux <desvaux.julie@gmail.com>
2023-02-01 12:45:32 +0100

    Add Method component in Offer page : Prendre de la hauteur

---
+++ src/lib/components/mollecules/Offer/Method.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Circle from '$lib/components/atoms/Circle/Circle.svelte';
+	export let title: string;
+	export let paragraph: string;
+	export let contents: string[];
+</script>
+
+<div class="bg-gray p-6 relative rounded-xl">
+	<div class="absolute top-4 z-0">
+		<Circle size="medium" />
+	</div>
+
+	<h3 class="z-50 my-6 ml-10 mj-h3 font-extralight top-4">{title}</h3>
+	<p>{paragraph}</p>
+	<ul class="mj-list--disc">
+		{#each contents as content}
+			<li>{content}</li>
+		{/each}
+	</ul>
+</div>
--- src/routes/offres-de-services/prendre-de-la-hauteur/+page.svelte
+++ src/routes/offres-de-services/prendre-de-la-hauteur/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Icon from '$lib/components/atoms/Icon/Icon.svelte';
+	import Method from '$lib/components/mollecules/Offer/Method.svelte';
 	import Need from '$lib/components/mollecules/Offer/Need.svelte';
 </script>
 
@@ -27,9 +28,9 @@
 <main class="md:p-0 md:0" id="main">
 	<section class="md:p-0 md:py-12 p-4">
 		<div class="container h-full md:px-8">
-			<h2 class="my-10">Votre besoin</h2>
+			<h2 class="my-8">Votre besoin</h2>
 			<p>J’ai besoin de prendre de la hauteur et d’agir sur :</p>
-			<div class="lg:gap-x-20 lg:gap-y-10 grid gap-y-5  lg:grid-cols-2 mt-6">
+			<div class="lg:gap-x-20 lg:gap-y-10 grid gap-y-5 lg:grid-cols-2 my-6">
 				<Need
 					title="Ma stratégie RSE"
 					contents={[
@@ -49,6 +50,46 @@
 						'Comment mobiliser les forces de vente ?'
 					]}
 				/>
+			</div>
+		</div>
+	</section>
+	<section class="bg-gray md:p-0 md:py-12 p-4">
+		<div class="container h-full md:px-8">
+			<h2 class="my-8">Nos méthodes</h2>
+			<div class="md:grid md:grid-cols-5 gap-4">
+				<div class="col-span-3">
+					<Method
+						title="Diagnostic"
+						paragraph="Recueillir et analyser les attentes et perceptions de vos parties prenantes (collaborateurs, clients, fournisseurs...). Evaluer votre politique RSE et votre politique de communication associée, votre environnement concurrentiel"
+						contents={[
+							'Enquête par entretiens ou questionnaire',
+							'Cartographie des parties prenantes',
+							'Benchmark'
+						]}
+					/>
+				</div>
+				<div class="col-start-3 col-end-6">
+					<Method
+						title="Stratégie"
+						paragraph="Clarifier et prioriser vos enjeux environnementaux, sociaux et économiques, enrichir vos stratégies RSE et Marketing en cohérence avec le modèle d’affaire"
+						contents={[
+							'Ateliers de co-construction',
+							'Séminaires de restitution',
+							'Matrice de matérialité'
+						]}
+					/>
+				</div>
+				<div class="col-span-3">
+					<Method
+						title="Mobilisation et communication"
+						paragraph="Traduire vos engagements dans tous vos métiers et vos actions de communication"
+						contents={[
+							'Argumentaire',
+							'Réseau d’ambassadrices et d’ambassadeurs',
+							'Accompagnement des forces de vente'
+						]}
+					/>
+				</div>
 			</div>
 		</div>
 	</section>
