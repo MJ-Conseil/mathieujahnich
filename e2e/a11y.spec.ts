47f75af173808be11eb8b5aad5b48e35b6da39cc
julie-desvaux <desvaux.julie@gmail.com>
2023-02-06 18:22:20 +0100

    Add test e2e for offer page

--- e2e/a11y.spec.ts
+++ e2e/a11y.spec.ts
@@ -21,4 +21,14 @@ test.describe('accessibilty - pages without required authentication', () => {
 		const accessibilityScanResults = await makeAxeBuilder().analyze();
 		expect(accessibilityScanResults.violations).toEqual([]);
 	});
+
+	test('offres/prendre de la hauteur page should not have any automatically detectable accessibility issues', async ({
+		page,
+		makeAxeBuilder
+	}) => {
+		await page.goto('/offres-de-services/prendre-de-la-hauteur');
+
+		const accessibilityScanResults = await makeAxeBuilder().analyze();
+		expect(accessibilityScanResults.violations).toEqual([]);
+	});
 });
