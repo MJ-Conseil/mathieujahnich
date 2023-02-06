59c1aff90211b38a86ac424c561aa5a691483635
volubyl <volubyl@pm.me>
2023-02-06 17:36:25 +0100

    WIO

---
+++ src/lib/components/atoms/Tag/Tag.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<button
+	class="border border-blue-dark p-1  min-w-[50px] hover:bg-blue-dark hover:text-white  rounded-xl text-blue-dark"
+	on:click
+>
+	{name}
+</button>
--- src/routes/references/+page.svelte
+++ src/routes/references/+page.svelte
@@ -1,7 +1,17 @@
 <script>
-	import { Container } from 'postcss';
+	import Container from '$lib/components/atoms/Container/Container.svelte';
 </script>
 
 <Container>
-	<h1>Références</h1>
+	<header class="mt-8">
+		<h1>Références</h1>
+	</header>
 </Container>
+
+<main>
+	<section>
+		<Container>
+			<h2 />
+		</Container>
+	</section>
+</main>
