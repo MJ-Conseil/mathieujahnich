2ff11abf88fda65bf6d9a2d35d8921ce4511dac4
volubyl <volubyl@pm.me>
2023-02-21 13:31:22 +0100

    refacto: use a Section component to fix section padding and marging

--- src/routes/offres-de-services/prendre-de-la-hauteur/+page.ts
+++ src/routes/offres-de-services/prendre-de-la-hauteur/+page.ts
@@ -1,27 +1,21 @@
-
 import { getOfferTypes } from '$lib/repositories/offerTypes';
 import { getReferences } from '$lib/repositories/reference';
 import type { PageLoad } from './$types';
 
-export const load: PageLoad = async ({ fetch, params }) => {
+export const load: PageLoad = async ({ fetch }) => {
+	const offerTypes = await getOfferTypes(fetch);
 
-    const offerTypes = await getOfferTypes(fetch);
+	const offerType = offerTypes.find((item) => item.slug === 'prendre-de-la-hauteur');
 
-    const offerType = offerTypes.find(item => item.slug === "prendre-de-la-hauteur")
+	if (!offerType) {
+		return { references: [] };
+	}
 
-    if (!offerType) {
-        return { references: [] }
-    }
+	const references = await getReferences(fetch, {
+		highlight: 1,
+		per_page: 3,
+		offerTypeId: offerType.id
+	});
 
-
-    const references = await getReferences(fetch, {
-        highlight: 1,
-        per_page: 3,
-        offerTypeId: offerType.id
-    })
-
-
-
-    return { references }
-
+	return { references };
 };
