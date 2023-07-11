08f9c7bf3e4d675c71b1df1e3889a3046bba3df3
volubyl <volubyl@pm.me>
2023-07-11 14:22:36 +0200

    fix: few ui fixes

--- src/lib/components/mollecules/Offer/Offer.svelte
+++ src/lib/components/mollecules/Offer/Offer.svelte
@@ -1,20 +1,20 @@
 <script lang="ts">
-	import type { IconNames } from 'src/definitions';
 	import ArrowLink from '../ArrowLink/ArrowLink.svelte';
-	export let title: string;
+	export let offerName: string;
 	export let offer: string;
 	export let link: string = '';
+	export let linkTitle: string = '';
 </script>
 
 <div class="h-full w-full flex flex-col justify-evenly relative">
 	<slot />
-	<h3 class="mb-4">{title}</h3>
+	<h3 class="my-5">{offerName}</h3>
 	<p class="min-h-[150px]">
 		{offer}
 	</p>
 	{#if link}
 		<div class="mt-6">
-			<ArrowLink href={link}>Lire la suite</ArrowLink>
+			<ArrowLink {linkTitle} href={link}>Lire la suite</ArrowLink>
 		</div>
 	{/if}
 </div>
--- src/lib/components/organisms/OfferRow.svelte
+++ src/lib/components/organisms/OfferRow.svelte
@@ -1,8 +1,7 @@
 <script lang>
-	import { ROUTES, SIZE } from '$lib/constants';
-	import Circle from '../atoms/Circle/Circle.svelte';
-	import Icon from '../atoms/Icon/Icon.svelte';
+	import { ROUTES } from '$lib/constants';
 	import Offer from '../mollecules/Offer/Offer.svelte';
+	import OfferIcon from '../mollecules/OfferIcon/OfferIcon.svelte';
 	import Section from '../mollecules/Section/Section.svelte';
 
 	export let alt = false;
@@ -12,41 +11,29 @@
 	<h2 class="mb-8">Nos offres de services</h2>
 	<div class="md:gap-x-20 md:gap-y-10 grid gap-y-12 md:grid-cols-3 pt-8">
 		<Offer
-			title="Donner de l’élan"
+			offerName="Donner de l’élan"
+			linkTitle="Lire la suite offre : donner de l'élan"
 			offer="Dynamiser les stratégies RSE et la communication des entreprises à travers l’analyse du positionnement et des principaux enjeux environnementaux et sociaux, l’étude de l’environnement concurrentiel, le recueil des attentes des parties-prenantes."
 			link={ROUTES["Donner de l'élan"]}
 		>
-			<div aria-hidden="true" class="relative text-blue-dark h-[200px]">
-				<Circle size={SIZE.REGULAR} />
-				<div class="absolute left-[25px]  md:left-[80px]  top-[40px]">
-					<Icon height="150px" width="150px" name="plane" fill="currentColor" />
-				</div>
-			</div>
+			<OfferIcon iconName="plane" />
 		</Offer>
 
 		<Offer
-			title="Trouver le juste équilibre"
+			linkTitle="Lire la suite offre : Trouver le juste équilibre"
+			offerName="Trouver le juste équilibre"
 			offer="Analyser les allégations environnementales et sociales des supports marketing et communication en regard du cadre déontologique et réglementaire et mettre en place des filets de sécurité pour limiter les critiques (greenwashing, stéréotypes…)."
 			link={ROUTES['Trouver le juste équilibre']}
 		>
-			<div aria-hidden="true" class="relative text-blue-dark h-[200px]">
-				<Circle size={SIZE.REGULAR} />
-				<div class="absolute left-[40px]  md:left-[80px]  top-[30px]">
-					<Icon height="150px" width="150px" name="stones" fill="currentColor" />
-				</div>
-			</div>
+			<OfferIcon iconName="stones" />
 		</Offer>
 		<Offer
-			title="Faire monter en compétences"
+			linkTitle="Lire la suite offre : Faire monter en compétences"
+			offerName="Faire monter en compétences"
 			offer="Animer des formations et concevoir des dispositifs pour sensibiliser et accompagner les fonctions marketing, pub, com et toutes leurs interfaces (autres directions, business units, partenaires…), vers des pratiques plus responsables."
 			link={ROUTES['Faire monter en compétences']}
 		>
-			<div aria-hidden="true" class="relative text-blue-dark h-[200px]">
-				<Circle size={SIZE.REGULAR} />
-				<div class="absolute left-[40px]  md:left-[80px]  top-[30px]">
-					<Icon height="150px" width="150px" name="ladder" fill="currentColor" />
-				</div>
-			</div>
+			<OfferIcon iconName="ladder" />
 		</Offer>
 	</div>
 </Section>
