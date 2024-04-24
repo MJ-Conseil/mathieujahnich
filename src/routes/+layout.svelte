d256feb3f55b249b9e1e365c2d1bc23365046011
volubyl <volubyl@pm.me>
2024-04-24 15:06:05 +0200

    WIP

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,13 @@
 	// note : this is a bit hacking ... After navigating to a page with a "subroute" (aka notre-offre/xxxx) the focus was not correctly reset
 	// this code ensure that ...
 	afterNavigate((navigation) => {
-		node.focus();
+		console.log({
+			from: navigation.from?.url,
+			to: navigation.to?.url
+		});
+		if (navigation.from?.url.pathname !== navigation.to?.url.pathname) {
+			node.focus();
+		}
 	});
 </script>
 
